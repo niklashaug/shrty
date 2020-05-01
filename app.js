67e861f310e75959a6b4b04141ec93f4a031bd52
Niklas Haug <niklas@nhaug.de>
2020-05-01 13:52:26 +0200

    expanded validation (escaping user content before modifying database)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path')
 const shurley = require('shurley')
 const session = require('express-session')
 const bcrypt = require('bcrypt')
+const { body, validationResult } = require('express-validator')
 
 // SEQUELIZE CONFIG
 const config = require('./config')
@@ -92,6 +93,35 @@ app.use(session({
     secure: process.env.NODE_ENV === 'production'
 }))
 
+// VALIDATOR
+const validate = validations => {
+    return async (req, res, next) => {
+        await Promise.all(validations.map(validation => validation.run(req)));
+
+        //const errors = validationResult(req)
+
+        const customValidationResult = validationResult.withDefaults({
+            formatter: error => {
+                return {
+                    field: error.param,
+                    message: error.msg
+                }
+            }
+        })
+
+        const errors = customValidationResult(req)
+
+
+        if (errors.isEmpty()) {
+            return next();
+        }
+
+        res.status(422).json({
+            errors: errors.array()
+        })
+    };
+}
+
 // AUTHENTICATION MIDDLEWARE
 async function AuthenticationPolicy (req, res, next) {
     if(req.session && req.session.user && req.session.user.ID) {
@@ -142,12 +172,15 @@ app.get('/register', (req, res) => {
     })
 })
 
-app.post('/register', csrfPolicy, async (req, res) => {
+app.post('/register', csrfPolicy, validate([
+    body('username').isLength({ min: 5 }).withMessage("Username must have at least 5 characters.").escape(),
+    body('password').isLength({ min: 8}).withMessage("Password must have at least 8 characters.")
+]), async (req, res) => {
     const hash = await bcrypt.hash(req.body.password, 10)
 
     try {
         const user = await User.create({
-            user: req.body.user,
+            username: req.body.username,
             password: hash
         })
 
@@ -155,10 +188,17 @@ app.post('/register', csrfPolicy, async (req, res) => {
             res.status(200).redirect('/login')
         }
     } catch(err) {
+        console.log(err)
         if(err.name === 'SequelizeUniqueConstraintError') {
             req.session.csrf = cryptoRandomString({ length: config.csrf.tokenLength })
             res.render('register', {
                 error: 'This username has already been taken.',
+                csrfToken: req.session.csrf
+            })
+        } else if(err.name === 'SequelizeValidationError') {
+            req.session.csrf = cryptoRandomString({ length: config.csrf.tokenLength })
+            res.render('register', {
+                error: err.message,
                 csrfToken: req.session.csrf
             })
         }
@@ -174,16 +214,19 @@ app.get('/login', (req, res) => {
         csrfToken: req.session.csrf
     })
 })
-
-app.post('/login', csrfPolicy, async (req, res) => {
+// <h1>myfancyusername</h1><script>window.alert('boese')</script> -> myfancypassword
+app.post('/login', csrfPolicy, validate([
+    body('username').escape()
+]), async (req, res) => {
+    console.log(req.body.username)
     const user = await User.findOne({
         where: {
             username: req.body.username
-        },
-        include: [URL]
+        }
     })
 
     if(user && user.activated) {
+        console.log("FOUND USER")
         if(await bcrypt.compare(req.body.password, user.password)) {
             //success
             req.session.user = {
--- config.js
+++ config.js
@@ -12,5 +12,11 @@ module.exports = {
         port: process.env.DB_PORT || '3306',
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD
-    }
+    },
+    /*mail: {
+        host: process.env.MAIL_HOST || 'localhost',
+        port: process.env.MAIL_PORT || 465,
+        user: process.env.MAIL_USER,
+        password: process.env.MAIL_PASSWORD
+    }*/
 }
